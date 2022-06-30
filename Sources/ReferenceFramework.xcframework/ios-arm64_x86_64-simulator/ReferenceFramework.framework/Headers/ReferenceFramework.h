30c7bd262262d8c0c4b36f606be17a6073ba143b
Vinisha Nagarajan <vinisha99@gmail.com>
2022-06-30 14:13:32 -0400

    Initial Commit

---
+++ Sources/ReferenceFramework.xcframework/ios-arm64_x86_64-simulator/ReferenceFramework.framework/Headers/ReferenceFramework.h
@@ -0,0 +1,18 @@
+//
+//  ReferenceFramework.h
+//  ReferenceFramework
+//
+//  Created by Vinisha Nagarajan on 6/30/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReferenceFramework.
+FOUNDATION_EXPORT double ReferenceFrameworkVersionNumber;
+
+//! Project version string for ReferenceFramework.
+FOUNDATION_EXPORT const unsigned char ReferenceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReferenceFramework/PublicHeader.h>
+
+
---
+++ Sources/SwiftPackage/SwiftPackage.swift
@@ -0,0 +1,6 @@
+public struct SwiftPackage {
+    public private(set) var text = "Hello, World from Swift Package"
+
+    public init() {
+    }
+}
